ffd13fa02e56e250abaed9f6419605889147cf82
murn0 <xx.naoyamurano.xx@gmail.com>
2024-07-11 15:58:31 +0900

    feat(plugin): add mini.tabline

---
+++ lua/plugins/mini-tabline.lua
@@ -0,0 +1,9 @@
+return function()
+  local tabline = require("mini.tabline")
+  tabline.setup({
+    format = function(buf_id, label)
+      local suffix = vim.bo[buf_id].modified and "+ " or ""
+      return tabline.default_format(buf_id, label) .. suffix
+    end,
+  })
+end
