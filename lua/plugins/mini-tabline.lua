f1bd00908df0fba3703ceed36935b39d598a9edc
murn0 <xx.naoyamurano.xx@gmail.com>
2024-07-11 23:45:26 +0900

    feat(plugin): add mini.tabline

--- lua/plugins/mini-tabline.lua
+++ lua/plugins/mini-tabline.lua
@@ -1,9 +1,45 @@
 return function()
   local tabline = require("mini.tabline")
-  tabline.setup({
-    format = function(buf_id, label)
-      local suffix = vim.bo[buf_id].modified and "+ " or ""
-      return tabline.default_format(buf_id, label) .. suffix
-    end,
+  local palette = require("nightfox.palette").load("nightfox")
+
+  vim.api.nvim_set_hl(0, "MiniTablineCurrent", {
+    fg = palette.fg1,
+    bg = palette.bg1,
+    bold = true,
   })
+
+  vim.api.nvim_set_hl(0, "MiniTablineVisible", {
+    fg = palette.fg2,
+    bg = palette.bg3,
+  })
+
+  vim.api.nvim_set_hl(0, "MiniTablineHidden", {
+    fg = palette.fg3,
+    bg = "#363636",
+  })
+
+  vim.api.nvim_set_hl(0, "MiniTablineModifiedCurrent", {
+    fg = palette.yellow.base,
+    bg = palette.bg1,
+    bold = true,
+    italic = true,
+  })
+
+  vim.api.nvim_set_hl(0, "MiniTablineModifiedVisible", {
+    fg = palette.yellow.dim,
+    bg = palette.bg3,
+    italic = true,
+  })
+
+  vim.api.nvim_set_hl(0, "MiniTablineModifiedHidden", {
+    fg = palette.yellow.dim,
+    bg = "#363636",
+    italic = true,
+  })
+
+  vim.api.nvim_set_hl(0, "MiniTablineFill", {
+    bg = palette.bg1,
+  })
+
+  tabline.setup()
 end
