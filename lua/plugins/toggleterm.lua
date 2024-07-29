6a7a18826a13c0677ad3abd7554e7c458f8208c1
murn0 <xx.naoyamurano.xx@gmail.com>
2024-07-29 23:43:30 +0900

    feat: add toggleterm settings

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,11 +1,60 @@
 return function()
   local toggleterm = require("toggleterm")
+  local Terminal = require("toggleterm.terminal").Terminal
+  local pick = require("mini.pick")
 
-  vim.keymap.set("n", "<leader>t", function()
-    toggleterm.toggle_command("direction=float", vim.v.count)
-  end, { desc = "Toggle terminal" })
+  --[[
+  -- Keymaps
+  --]]
+  local keymap = vim.keymap
 
-  toggleterm.setup({})
+  -- ターミナルモード用のkeymapを設定
+  function _G.set_terminal_keymaps()
+    local opts = { buffer = 0 }
+    keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+    keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+    keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+    keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+    keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+    keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+  end
+
+  -- ToggleTerm以外の端末モードでもこのkeymapを使用したい場合は`term://*`に設定する
+  vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
+
+  -- <M-¥>にToggleTermのコマンドを表示するkeymapを設定
+  keymap.set("n", "\\", function()
+    local items = {
+      { value = "vertical", text = "Show vertical" },
+      { value = "horizontal", text = "Show horizontal" },
+      { value = "float", text = "Show float" },
+      { value = "tab", text = "Show tab" },
+      { value = "all", text = "Show all terminals" },
+      { value = "select", text = "Select terminal" },
+      { value = "name", text = "Set terminal name" },
+    }
+
+    local choose = function(item)
+      local value = item.value
+      local keys = ""
+      if value == "all" then
+        keys = ":ToggleTermToggleAll\n"
+      elseif value == "select" then
+        keys = ":TermSelect\n"
+      elseif value == "name" then
+        keys = ":ToggleTermSetName\n"
+      else
+        keys = ":ToggleTerm direction=" .. value .. "\n"
+      end
+
+      vim.schedule(function()
+        vim.fn.feedkeys(keys)
+      end)
+    end
+
+    pick.start({ source = { items = items, name = "Select ToggleTerm commands", choose = choose } })
+  end, { desc = "Select ToggleTerm commands" })
+
   -- Lazygitの設定
   local lazygit = Terminal:new({
     cmd = "lazygit",
@@ -38,4 +87,18 @@ return function()
   keymap.set("n", "<Leader>l", function()
     lazygit:toggle()
   end, { desc = "Open LazyGit" })
+
+  --[[
+  -- Configurations
+  --]]
+  toggleterm.setup({
+    size = function(args)
+      if args.direction == "horizontal" then
+        return 17
+      elseif args.direction == "vertical" then
+        return vim.o.columns * 0.4
+      end
+    end,
+    open_mapping = { [[<c-\>]], [[<c-¥>]] },
+  })
 end
